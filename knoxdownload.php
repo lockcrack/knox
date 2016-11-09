c20b9754991e7fb704a52cddb078bcd885bafae3
Miguel Piedrafita <github@miguelpiedrafita.com>
2016-11-09 10:43:00 +0100

    Update knoxdownload.php

--- knoxdownload.php
+++ knoxdownload.php
@@ -1,4 +1,4 @@
 <?php
-
+include ('config.php');
 $data = json_decode(file_get_contents('https://aas1541.cellwe.samsungknox.com/IosAppRest/GetClientUpdateInfo?deviceId='.$deviceId));
 echo $data->Url[0];

6f5f1f4be4c8cbd211f5e3bfb24e5d3261cf8428
Miguel Piedrafita <github@miguelpiedrafita.com>
2016-11-09 11:36:21 +0100

    Update knoxdownload.php

--- knoxdownload.php
+++ knoxdownload.php
@@ -2,4 +2,9 @@
 
 include 'config.php';
 $data = json_decode(file_get_contents($serverUrl.'/IosAppRest/GetClientUpdateInfo?deviceId='.$deviceId));
-echo $data->Url;
+if ($data->Status == true ){
+header ("Location: ".$data->Url);
+} else {
+echo "Error: No se ha podido acceder a los datos. Comprueba que has seguido las <a href='https://github.com/m1guelpiedrafita/knoxapi'>instrucciones de instalaci&oacute;n</a>";
+exit;
+}
